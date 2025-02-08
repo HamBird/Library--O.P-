5c7ff622794487cd7bb5a1ebc2c31aae07a7860c
HamBird <122409142+HamBird@users.noreply.github.com>
2025-02-08 13:26:07 -0700

    Editted Function, Fixed Styling
    Added code to displayBookToLibrary() to add cards, fixed styling to prevent crushing on cards, header is in fixed position

--- library.js
+++ library.js
@@ -1,7 +1,9 @@
 console.log("Help");
 
+// Array to store all books in the library
 const myLibrary = [];
 
+// object constructor to hold relevant data for each book
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -15,7 +17,45 @@ function Book(title, author, pages, isRead) {
     }
 }
 
+var testBook = new Book("Hobbit", "Unknown Author", 52, false);
+myLibrary.push(testBook);
+{/* 
+<div class="books">
+    <div class="book">
+        <p><span>Unknown Book</span> by Unknown Author</p>
+        <div>
+            <p>256 pages</p>
+            <p>book read</p>
+        </div>
+        <div>
+            <input type="button" value="Delete">
+            <input type="button" value="Read">
+        </div>
+    </div>
+</div> */}
+
+// Function to create a new book DOM and display to html
+function displayBookToLibrary() {
+    let bookContainer = document.querySelector(".books");
+    // bookContainer.innerHTML = "";
+
+    for (let index = 0; index < myLibrary.length; index++) {
+        let book = document.createElement("div");
+        book.setAttribute("data-index", index);
+        book.classList.add("book");
+
+        var bookDetails = "";
+        bookDetails += `<p><span>${myLibrary[index].title}</span> by ${myLibrary[index].author}</p>`;
+        bookDetails += `<div><p>${myLibrary[index].pages} pages</p><p>${myLibrary[index].isRead ? "Read" : "Not Read"}</p></div>`;
+        bookDetails += `<div><input type="button" value="Delete"><input type="button" value="Read"></div>`;
+        book.innerHTML = bookDetails;
+        bookContainer.appendChild(book);
+    }
+}
+
+// Function to add a new book to book array from user
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
+
