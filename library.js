256c93b96523f555bb02d86b40c8261a55b34c01
HamBird <122409142+HamBird@users.noreply.github.com>
2025-02-08 09:16:46 -0700

    Added Files, Skeleton Code

---
+++ library.js
@@ -0,0 +1,21 @@
+console.log("Help");
+
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? "read" : "not read yet"}`;
+    }
+    this.read = function () {
+        this.isRead = !this.isRead;
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
