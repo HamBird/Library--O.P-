8e5b987ef7c9f5babd8c963cb50c6dffdb1eac61
HamBird <122409142+HamBird@users.noreply.github.com>
2025-02-09 18:27:59 -0700

    Added btn functionality
    implemented remove buttons to handle the deletion of books from the array, and created functionality for the read buttons to toggle if a book has been read

--- library.js
+++ library.js
@@ -1,5 +1,3 @@
-console.log("Help");
-
 // Array to store all books in the library
 const myLibrary = [];
 
@@ -13,19 +11,28 @@ function Book(title, author, pages, isRead) {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? "read" : "not read yet"}`;
     }
 }
-
-Book.prototype.read = function () {
+// prototype function to toggle read status
+Book.prototype.toggleread = function () {
     this.isRead = !this.isRead;
 }
 
-// var testBook = new Book("Hobbit", "Unknown Author", 52, false);
-// myLibrary.push(testBook);
+// Test Data for array
+var testBook = new Book("Hobbit", "Unknown Author", 52, false);
+myLibrary.push(testBook);
+var testBook = new Book("Bone", "Unknown Author", 521, true);
+myLibrary.push(testBook);
+var testBook = new Book("Diary of a wimpy kid", "Unknown Author", 122, true);
+myLibrary.push(testBook);
+var testBook = new Book("Book", "Unknown Author", 52, false);
+myLibrary.push(testBook);
 
 // Function to create a new book DOM and display to html
 function displayBookToLibrary() {
+    // fetches the books container and clears
     let bookContainer = document.querySelector(".books");
     bookContainer.innerHTML = "";
 
+    // iterates over each book in the array and creates html structure for a "card"
     for (let index = 0; index < myLibrary.length; index++) {
         let book = document.createElement("div");
         book.setAttribute("data-index", index);
@@ -34,10 +41,25 @@ function displayBookToLibrary() {
         var bookDetails = "";
         bookDetails += `<p><span>${myLibrary[index].title}</span> by ${myLibrary[index].author}</p>`;
         bookDetails += `<div><p>${myLibrary[index].pages} pages</p><p>${myLibrary[index].isRead ? "Read" : "Not Read"}</p></div>`;
-        bookDetails += `<div><input type="button" value="Delete"><input type="button" value="Read"></div>`;
+        bookDetails += `<div><input type="button" value="Delete" class="deletebtn" data-index="${index}"><input type="button" value="Read" class="readbtn" data-index="${index}"></div>`;
         book.innerHTML = bookDetails;
+        // appends book element to books container
         bookContainer.appendChild(book);
     }
+    // subscribe events to all delete and read buttons
+    var deleteBtns = document.querySelectorAll(".deletebtn");
+    var readBtns = document.querySelectorAll(".readbtn");
+    deleteBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            deleteBookFromLibrary(btn.dataset.index);
+        });
+    });
+    readBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            myLibrary[btn.dataset.index].toggleread();
+            displayBookToLibrary();
+        });
+    });
 }
 
 // Function to add a new book to book array from user
@@ -46,21 +68,44 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
     displayBookToLibrary();
 }
+// function to delete a book from the array using specified index
+function deleteBookFromLibrary(index) {
+    myLibrary.splice(index, 1);
+    displayBookToLibrary();
+}
 
-let addBook = document.getElementById("addBook");
-addBook.addEventListener('click', () => {
-    let bookTitle = document.getElementById("title").value;
-    let bookAuthor = document.getElementById("author").value;
-    let bookPages = document.getElementById("pages");
-    let bookRead = document.getElementById("isread").checked;
+// used to display books in array to html
+displayBookToLibrary();
 
-    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead);
-});
+// clears the data in the dialog
+function clearDialog() {
+    document.getElementById("title").value = '';
+    document.getElementById("author").value = '';
+    document.getElementById("pages").value = '';
+    document.getElementById("isread").checked = false;
+}
 
 let dialog = document.getElementById("book-dialog");
+// opens the dialog
 function openDialog() {
     dialog.showModal();
 }
+// clears the data in the dialog and closes after
 function closeDialog() {
+    clearDialog();
     dialog.close();
 }
+
+// creates an event listener for the addbook button in the dialog to submit user provided
+//  data to the library and closes the dialog
+let addBook = document.getElementById("addBook");
+addBook.addEventListener('click', (event) => {
+    event.preventDefault();
+    let bookTitle = document.getElementById("title").value;
+    let bookAuthor = document.getElementById("author").value;
+    let bookPages = +document.getElementById("pages").value;
+    let bookRead = document.getElementById("isread").checked;
+
+    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead);
+    closeDialog();
+});
